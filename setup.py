62e849af310b086de95a89a298f26b77a0370651
Gesli <gesli.medina@gmail.com>
2022-03-12 20:09:00 -0300

    Criando setup.py para preparar o envio para pypi

--- setup.py
+++ setup.py
@@ -94,8 +94,8 @@ def find_package_data(
     return out
 
 
-PACKAGE = "libpythonpro_gesli"
-NAME = PACKAGE
+PACKAGE = "libpythonpro"
+NAME = "libpythonpro-gesli"
 DESCRIPTION = "Módulo para exemplificar construção de projetos Python no curso PyTools"
 AUTHOR = "Gesli Medina de Avelar"
 AUTHOR_EMAIL = "gesli.medina@gmail.com"
@@ -110,7 +110,7 @@ setup(
     long_description_content_type='text/markdown',
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
-    license=read('LICENSE'),
+    license="GNU AFFERO GENERAL PUBLIC LICENSE",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
     package_data=find_package_data(PACKAGE, only_in_packages=False),
