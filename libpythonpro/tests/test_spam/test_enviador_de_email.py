a68e621412f8a0369d880a4b070c0ed63c81bcfb
Gesli <gesli.medina@gmail.com>
2022-03-17 11:11:12 -0300

    TDD e Baby Steps

---
+++ libpythonpro/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,17 @@
+from libpythonpro.spam.enviador_de_email import Enviador
+
+
+def  test_criar_enviador_de_email():
+     enviador= Enviador()
+     assert enviador is not None
+
+
+def test_remetente():
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        'gesli.medina@gmail.com',
+        'santannathiago@gmail.com',
+        'Curso Python Pro',
+        'Primeira turma aberta.'
+    )
+    assert 'gesli.medina@gmail.com' in resultado
