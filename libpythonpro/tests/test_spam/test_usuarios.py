913f424ea95880ea211cab35ccb501ac85d1ede4
Gesli <gesli.medina@gmail.com>
2022-03-23 21:26:53 -0300

    Produção de Código Testável

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,16 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome ='Gesli')
+    usuario = Usuario(nome='Gesli', email='gesli.medina@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='Gesli'), Usuario(nome='Thiago')]
+    usuarios = [
+        Usuario(nome='Gesli', email='gesli.medina@gmail.com'),
+        Usuario(nome='Thiago', email='santannathiago@gmail.com')
+    ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
