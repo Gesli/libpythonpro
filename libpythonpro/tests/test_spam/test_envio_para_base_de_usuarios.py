d3f44f0d113719cb1cf2dc576ad3f6a971303d4d
Gesli <gesli.medina@gmail.com>
2022-03-23 21:26:53 -0300

    Produção de Código Testável

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,30 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam= EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Gesli', email='gesli.medina@gmail.com'),
+            Usuario(nome='Thiago', email='santannathiago@gmail.com')
+        ],
+        [
+            Usuario(nome='Gesli', email='gesli.medina@gmail.com')
+        ]
+    ]
+)
+def test_qde_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = Enviador()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
         'gesli.medina@gmail.com',
         'Curso Python Pro',
         'Confira os modulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
